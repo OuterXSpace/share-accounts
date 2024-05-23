1c69c3cf1d00db89c2201b8f56ca31f1decfa877
warjamma <lgold141@gmail.com>
2024-05-23 23:01:02 +0700

    refactor(home-page): update UI home

--- src/next-core-ui/share-account/src/components/product-list/product-list.tsx
+++ src/next-core-ui/share-account/src/components/product-list/product-list.tsx
@@ -3,44 +3,148 @@ import { IProductListProps } from './product-list.type';
 
 export const ProductList: React.FC<IProductListProps> = () => {
   return (
-    <div className="bg-white flex flex-col items-center mt-[30px] p-6 bg-[50% 50%]">
-      <h2 className="uppercase text-[30px]">
-        <strong>Sản phẩm </strong>
-        Bán chạy
-      </h2>
-      <div className="grid md:grid-cols-3 lg:grid-cols-4 gap-4 mt-[20px]">
-        <ProductCard
-          img="https://gamikey.com/wp-content/uploads/2022/03/Netflix-510x510.jpg.webp"
-          alt="product"
-          title="Mua Tài khoản Netflix Premium "
-          quantity={9810}
-          category="Giải trí"
-          price={149}
-        />
-        <ProductCard
-          img="https://gamikey.com/wp-content/uploads/2022/03/Netflix-510x510.jpg.webp"
-          alt="product"
-          title="Mua Tài khoản Netflix Premium "
-          quantity={9810}
-          category="Giải trí"
-          price={149}
-        />
-        <ProductCard
-          img="https://gamikey.com/wp-content/uploads/2022/03/Netflix-510x510.jpg.webp"
-          alt="product"
-          title="Mua Tài khoản Netflix Premium "
-          quantity={9810}
-          category="Giải trí"
-          price={149}
-        />
-        <ProductCard
-          img="https://gamikey.com/wp-content/uploads/2022/03/Netflix-510x510.jpg.webp"
-          alt="product"
-          title="Mua Tài khoản Netflix Premium "
-          quantity={9810}
-          category="Giải trí"
-          price={149}
-        />
+    <div className="container flex flex-col items-center mt-[30px] p-6 bg-[50% 50%]">
+      <div className="row">
+        <div className="col">
+          <div className="tt-home-page__section-header flex flex-col items-center gap-4">
+            <h4 className="tt-home-page__section-heading m-0 pd-0 text-[44px] font-bold text-accent">
+              Sản phẩm bán chạy
+            </h4>
+            <div className="tt-home-page__section-subheading max-w-[40%] text-lg text-center font-extralight leading-[1.25em] text-white">
+              Danh sách những sản phẩm theo xu hướng mà có thể bạn sẽ thích
+            </div>
+          </div>
+
+          <div className="grid md:grid-cols-3 lg:grid-cols-4 gap-4 pt-9">
+            <ProductCard
+              img="https://gamikey.com/wp-content/uploads/2022/03/Netflix-510x510.jpg.webp"
+              alt="product"
+              title="Mua Tài khoản Netflix Premium "
+              quantity={9810}
+              category="Giải trí"
+              price={149}
+            />
+            <ProductCard
+              img="https://gamikey.com/wp-content/uploads/2022/03/Spotify-2-510x510.png.webp"
+              alt="product"
+              title="Mua Tài khoản Netflix Premium "
+              quantity={9810}
+              category="Giải trí"
+              price={149}
+            />
+            <ProductCard
+              img="https://gamikey.com/wp-content/uploads/2022/03/Image-product-5-Youtube-510x510.png.webp"
+              alt="product"
+              title="Mua Tài khoản Netflix Premium "
+              quantity={9810}
+              category="Giải trí"
+              price={149}
+            />
+            <ProductCard
+              img="https://gamikey.com/wp-content/uploads/2024/05/4-1-510x510.png"
+              alt="product"
+              title="Mua Tài khoản Netflix Premium "
+              quantity={9810}
+              category="Giải trí"
+              price={149}
+            />
+            <ProductCard
+              img="https://gamikey.com/wp-content/uploads/2024/05/tinder-1-min-510x510.png"
+              alt="product"
+              title="Mua Tài khoản Netflix Premium "
+              quantity={9810}
+              category="Giải trí"
+              price={149}
+            />
+            <ProductCard
+              img="https://gamikey.com/wp-content/uploads/2024/04/Perplexity-Product-510x510.png"
+              alt="product"
+              title="Mua Tài khoản Netflix Premium "
+              quantity={9810}
+              category="Giải trí"
+              price={149}
+            />
+            <ProductCard
+              img="https://gamikey.com/wp-content/uploads/2023/12/movie-pass-510x510.jpeg"
+              alt="product"
+              title="Mua Tài khoản Netflix Premium "
+              quantity={9810}
+              category="Giải trí"
+              price={149}
+            />
+            <ProductCard
+              img="https://gamikey.com/wp-content/uploads/2024/04/Finale-2-Pro-510x510.png"
+              alt="product"
+              title="Mua Tài khoản Netflix Premium "
+              quantity={9810}
+              category="Giải trí"
+              price={149}
+            />
+
+            <ProductCard
+              img="https://gamikey.com/wp-content/uploads/2022/03/Netflix-510x510.jpg.webp"
+              alt="product"
+              title="Mua Tài khoản Netflix Premium "
+              quantity={9810}
+              category="Giải trí"
+              price={149}
+            />
+            <ProductCard
+              img="https://gamikey.com/wp-content/uploads/2022/03/Spotify-2-510x510.png.webp"
+              alt="product"
+              title="Mua Tài khoản Netflix Premium "
+              quantity={9810}
+              category="Giải trí"
+              price={149}
+            />
+            <ProductCard
+              img="https://gamikey.com/wp-content/uploads/2022/03/Image-product-5-Youtube-510x510.png.webp"
+              alt="product"
+              title="Mua Tài khoản Netflix Premium "
+              quantity={9810}
+              category="Giải trí"
+              price={149}
+            />
+            <ProductCard
+              img="https://gamikey.com/wp-content/uploads/2024/05/1-3-510x510.jpg"
+              alt="product"
+              title="Mua Tài khoản Netflix Premium "
+              quantity={9810}
+              category="Giải trí"
+              price={149}
+            />
+          </div>
+
+          <div className="flex flex-col items-center gap-4 pt-9">
+            <h4 className="tt-home-page__section-heading m-0 pd-0 text-[44px] font-bold text-accent">
+              Trải nghiệm ngay
+            </h4>
+            <div className="max-w-[40%] text-lg text-center font-extralight leading-[1.25em] text-white">
+              Bùng nổ deal giá rẻ với shareaccount.com
+            </div>
+          </div>
+
+          <div className="section-body">
+            <div className="grid grid-cols-2">
+              <div className="flex flex-col justify-center gap-2">
+                <h5 className="text-3xl font-bold text-accent">
+                  Mua tài khoản Netflix giá rẻ chính chủ Việt Nam ở đâu?
+                </h5>
+                <div className="text-lg font-light leading-[1.7em] text-white">
+                  Chúng tôi cung cấp duy nhất các tài khoản Netflix premium 4K, Full HD giá rẻ nhất thị trường, chính
+                  chủ ở Việt Nam. Giá chỉ từ 49.000đ 1 tháng, giúp bạn tối ưu chi phí.
+                </div>
+              </div>
+              <div className="flex items-center justify-center">
+                <img
+                  className="w-full p-10"
+                  src="https://muataikhoannetflixvn.com/wp-content/uploads/2024/04/KM-muataikhoannetflixvn.png"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/next-core-ui/share-account/src/views/menu-desktop/menu-desktop.views.tsx
+++ src/next-core-ui/share-account/src/views/menu-desktop/menu-desktop.views.tsx
@@ -1,172 +1,95 @@
 import React from 'react';
 import { IMenuDesktopProps } from './menu-desktop.type';
-import IonIcon from '@reacticons/ionicons';
 import Link from 'next/link';
+import IonIcon from '@reacticons/ionicons';
+
+const classNamePrefix = 'tt-header';
 
 export const MenuDesktop: React.FC<IMenuDesktopProps> = () => {
   return (
-    <nav className="menu-desktop w-full fixed top-0 z-50 bg-white">
-      <div className="w-screen flex flex-col items-center border-b border-slate-200 border-solid">
-        <div className="gap-[100px] flex flex-col sm:flex-row w-full items-center   xl:px-[300px] lg:px-[170px]">
-          <h1 className="font-semibold text-4xl text-red-600 uppercase">Netflix</h1>
-          <div className="hidden lg:block">
-            <ul className="flex justify-center items-center lg:gap-[50px] xl:gap-[100px] font-sm font-bold text-gray-600 w-full">
-              {/* trang chủ */}
-              <Link href="/">
-                <li className="relative group hover:text-red-600 cursor-pointer py-6">
-                  <div className="flex flex-col items-center gap-1">
-                    <IonIcon name="home-outline" className="text-[30px]" />
-                    <span className="uppercase text-[12px]">Trang chủ</span>
-                    <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-red-400 transition-all ease-in-out group-hover:w-full" />
-                  </div>
-                </li>
+    <nav className={`${classNamePrefix} w-full fixed top-0 z-50 bg-white`}>
+      <div className={`${classNamePrefix}__top-nav border-b border-primary bg-gradient-to-top-primary h-[72px]`}>
+        <div className="container flex items-center justify-between h-[72px]">
+          <div className={`${classNamePrefix}__logo h-[72px]`}>
+            <img src="https://79sodo.com/assets/home/wui/logo.gif" alt="logo icon" className="h-full" />
+          </div>
+          <div className="flex items-center space-x-3">
+            <Link
+              href="#"
+              className="inline-flex items-center justify-center px-4 py-3 font-semibold text-sm text-white bg-primary-dark rounded-xl"
+            >
+              <IonIcon className="pr-1 text-base" name="person-outline" />
+              Tài khoản
+            </Link>
+            <Link
+              href="#"
+              className="inline-flex items-center justify-center px-4 py-3 font-semibold text-sm text-dark bg-white rounded-xl"
+            >
+              Giỏ hàng
+              <IonIcon className="pl-1 text-base" name="cart-outline" />
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <div className={`${classNamePrefix}-menu-bar-nav bg-primary-dark`}>
+        <div className="container flex items-center justify-between h-[48px]">
+          <div className="row">
+            <div
+              className={`${classNamePrefix}-bar__container  px-4 flex justify-between items-center gap-8 mx-auto max-w-screen-xl w-full`}
+            >
+              <Link
+                href="/"
+                className="toto-popup-link header-menu-item header-menu-bar__menu-item relative flex items-center min-h-[48px] h-full"
+              >
+                <span
+                  className={`${classNamePrefix}-item__label min-w-max text-sm text-white text-opacity-90 uppercase font-semibold`}
+                >
+                  Trang chủ
+                </span>
               </Link>
-              {/* netflix */}
-              <Link href="/">
-                <li className="relative group hover:text-red-600 cursor-pointer py-6">
-                  <div className="flex flex-col items-center gap-1">
-                    <img
-                      src="https://gamikey.com/wp-content/uploads/2022/03/Netflix-e1689902508512-150x150.png.webp"
-                      alt="netflix icon"
-                      className="w-[30px]"
-                    />
-                    <span className="uppercase text-[12px]">Netflix</span>
-                    <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-red-400 transition-all ease-in-out group-hover:w-full" />
-                    <ul className="group-hover:flex absolute top-[6.3rem] shadow-lg rounded-xl hidden grid-cols-4 p-4 gap-4 border text-gray-400 font-normal bg-white z-10 lg:w-[60rem] xl:w-[70rem] xl:left-[-25.5rem] lg:left-[-30.5rem]">
-                      <li>
-                        <h3 className="border-b py-2 mb-4 text-gray-900 font-semibold">Electronics</h3>
-                        <ul className="flex flex-col items-start justify-start gap-2">
-                          <li>
-                            <span> Desktop </span>
-                          </li>
-                          <li>
-                            <span> Desktop </span>
-                          </li>
-                          <li>
-                            <span> Desktop </span>
-                          </li>
-                          <li>
-                            <span> Desktop </span>
-                          </li>
-                          <li>
-                            <span> Desktop </span>
-                          </li>
-                          <li>
-                            <img
-                              src="https://azureapis.blob.core.windows.net/share-account/assets/images/electronics-banner-1.jpg"
-                              alt="pic"
-                            />
-                          </li>
-                        </ul>
-                      </li>
-                      <li>
-                        <h3 className="border-b py-2 mb-4 text-gray-900 font-semibold">Men's</h3>
-                        <ul className="flex flex-col items-start justify-start gap-2">
-                          <li>
-                            <span> Desktop </span>
-                          </li>
-                          <li>
-                            <span> Desktop </span>
-                          </li>
-                          <li>
-                            <span> Desktop </span>
-                          </li>
-                          <li>
-                            <span> Desktop </span>
-                          </li>
-                          <li>
-                            <span> Desktop </span>
-                          </li>
-                          <li>
-                            <img
-                              src="https://azureapis.blob.core.windows.net/share-account/assets/images/electronics-banner-2.jpg"
-                              alt="pic"
-                            />
-                          </li>
-                        </ul>
-                      </li>
-                      <li>
-                        <h3 className="border-b py-2 mb-4 text-gray-900 font-semibold">Women's</h3>
-                        <ul className="flex flex-col items-start justify-start gap-2">
-                          <li>
-                            <span> Desktop </span>
-                          </li>
-                          <li>
-                            <span> Desktop </span>
-                          </li>
-                          <li>
-                            <span> Desktop </span>
-                          </li>
-                          <li>
-                            <span> Desktop </span>
-                          </li>
-                          <li>
-                            <span> Desktop </span>
-                          </li>
-                          <li>
-                            <img
-                              src="https://azureapis.blob.core.windows.net/share-account/assets/images/mens-banner.jpg"
-                              alt="pic"
-                            />
-                          </li>
-                        </ul>
-                      </li>
-                      <li>
-                        <h3 className="border-b py-2 mb-4 text-gray-900 font-semibold">Electronics</h3>
-                        <ul className="flex flex-col items-start justify-start gap-2">
-                          <li>
-                            <span> Desktop </span>
-                          </li>
-                          <li>
-                            <span> Desktop </span>
-                          </li>
-                          <li>
-                            <span> Desktop </span>
-                          </li>
-                          <li>
-                            <span> Desktop </span>
-                          </li>
-                          <li>
-                            <span> Desktop </span>
-                          </li>
-                          <li>
-                            <img
-                              src="https://azureapis.blob.core.windows.net/share-account/assets/images/womens-banner.jpg"
-                              alt="pic"
-                            />
-                          </li>
-                        </ul>
-                      </li>
-                    </ul>
-                  </div>
-                </li>
+              <Link
+                href="/"
+                className="toto-popup-link header-menu-item header-menu-bar__menu-item relative flex items-center min-h-[48px] h-full"
+              >
+                <span
+                  className={`${classNamePrefix}-item__label min-w-max text-sm text-white text-opacity-90 uppercase font-semibold`}
+                >
+                  Phim ảnh
+                </span>
               </Link>
-              {/* tài khoản */}
-              <Link href="/">
-                <li className="relative group hover:text-red-600 cursor-pointer py-6">
-                  <div className="flex flex-col items-center gap-1">
-                    <IonIcon name="person-outline" className="text-[30px]" />
-                    <span className="uppercase text-[12px]">Tài khoản</span>
-                    <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-red-400 transition-all ease-in-out group-hover:w-full" />
-                  </div>
-                </li>
+              <Link
+                href="/"
+                className="toto-popup-link header-menu-item header-menu-bar__menu-item relative flex items-center min-h-[48px] h-full"
+              >
+                <span
+                  className={`${classNamePrefix}-item__label min-w-max text-sm text-white text-opacity-90 uppercase font-semibold`}
+                >
+                  Âm nhạc
+                </span>
               </Link>
-              {/* giỏ hàng */}
-              <Link href="/">
-                <li className="relative group hover:text-red-600 cursor-pointer py-6">
-                  <div className="flex flex-col items-center">
-                    <div className="relative">
-                      <span className="text-xs text-center font-semibold text-white absolute -top-2 -right-2 w-4 h-4 bg-red-400 rounded-full">
-                        0
-                      </span>
-                      <IonIcon name="bag-handle-outline" className="text-[30px]" />
-                    </div>
-                    <span className="uppercase text-[12px]">Giỏ hàng</span>
-                    <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-red-400 transition-all ease-in-out group-hover:w-full" />
-                  </div>
-                </li>
+              <Link
+                href="/"
+                className="toto-popup-link header-menu-item header-menu-bar__menu-item relative flex items-center min-h-[48px] h-full"
+              >
+                <span
+                  className={`${classNamePrefix}-item__label min-w-max text-sm text-white text-opacity-90 uppercase font-semibold`}
+                >
+                  Gải trí
+                </span>
               </Link>
-            </ul>
+              <Link
+                href="/"
+                className="toto-popup-link header-menu-item header-menu-bar__menu-item relative flex items-center min-h-[48px] h-full"
+              >
+                <IonIcon className="pr-1 text-base text-white" name="gift-outline" />
+                <span
+                  className={`${classNamePrefix}-item__label min-w-max text-sm text-white text-opacity-90 uppercase font-semibold`}
+                >
+                  Khuyến mãi
+                </span>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
