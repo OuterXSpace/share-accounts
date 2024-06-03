038f95be98959278be5934ee3f98cd7de30e0e0f
warjamma <lgold141@gmail.com>
2024-06-04 00:17:32 +0700

    chore: import cdn tailwind

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import { THEME } from '../constants/platform';
 
 const Document = () => {
   return (
     <Html lang="en">
       <Head>
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        {['DYNAMIC_URL_THEME_01'].includes(THEME) && <script src="https://cdn.tailwindcss.com" async />}
       </Head>
       <body className="overflow-x-hidden">
         <Main />
