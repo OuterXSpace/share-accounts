acdc779e78e1063a3ba71985526742b1cb97a45c
warjamma <lgold141@gmail.com>
2024-06-05 18:31:13 +0700

    feture: handle submit contact with sheet google

---
+++ src/api/save-google/index.ts
@@ -0,0 +1,1 @@
+export * from './post-google-sheet.api';
--- src/next-core-ui/landing-page/src/core-ui/v1/components/contact/contact.component.tsx
+++ src/next-core-ui/landing-page/src/core-ui/v1/components/contact/contact.component.tsx
@@ -1,14 +1,48 @@
 import IonIcon from '@reacticons/ionicons';
-import { ContactProps } from './contact.type';
+import { ContactProps, ILandingPageContactFormModel } from './contact.type';
 import { LandingPageButtonV1 } from '../../common';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { useCallback } from 'react';
+import { postGoogleSheetApi } from '../../../../../../../api/save-google';
 
 export const Contact: React.FC<ContactProps> = (props) => {
   const { data, className } = props;
 
+  const contactSchema = yup.object().shape({
+    fullName: yup.string().required(data?.object?.form?.input.FULL_NAME?.error),
+    email: yup.string().required(data?.object?.form?.input.EMAIL?.error),
+    phone: yup.string().required(data?.object?.form?.input.PHONE?.error),
+    message: yup.string().required(data?.object?.form?.input.MESSAGE?.error),
+  });
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    clearErrors,
+  } = useForm<ILandingPageContactFormModel>({
+    mode: 'onBlur',
+    reValidateMode: 'onBlur',
+    resolver: yupResolver(contactSchema),
+    defaultValues: {},
+  });
+
+  const onConfirmSubmit = useCallback((values: ILandingPageContactFormModel) => {
+    postGoogleSheetApi(values, data?.object?.apiGoogleSheetUrl)
+      .then(() => {
+        console.log('Data posted successfully!');
+      })
+      .catch((error) => {
+        console.error('Error posting data:', error);
+      });
+  }, []);
+
   return (
     <section className={`section-01 ${className}`}>
       <div className="container pb-[50px] md:pb-[100px] lg:pb-[150px]">
-        <div className="row">
+        <form className="row" onSubmit={handleSubmit(onConfirmSubmit)}>
           <div className="col-12 lg:pr-[50px] pr-0">
             <h1 className="text-[#f9fdfe] text-[44px] lg:text-[5.125rem] font-bold font-montserrat leading-1.4">
               {data?.object?.title}
@@ -35,7 +69,7 @@ export const Contact: React.FC<ContactProps> = (props) => {
           </div>
           <div className="lg:col-6 col-12 lg:pl-[50px] pl-0">
             <div className="mb-[20px] pt-[50px] lg:pl-[50px] px-1">
-              <form className="w-full">
+              <div className="w-full">
                 <div className="mb-[30px] relative">
                   <input
                     style={{
@@ -43,18 +77,61 @@ export const Contact: React.FC<ContactProps> = (props) => {
                     }}
                     type="text"
                     className="outline-none shadow-none text-[#666] box-border w-full bg-[#fafafa00] h-[58px] rounded-0 border-[#f9fdfe] border-b-2 border-t-0 border-l-0 border-r-0 focus:border-b-2 focus:border-[#96c583] focus:placeholder-[#96c583]"
-                    placeholder="Full Name"
+                    placeholder={data?.object?.form?.input.FULL_NAME?.label}
+                    {...register('fullName')}
+                    onChange={(e) => {
+                      if (!e.target.value) {
+                        clearErrors('fullName');
+                      }
+                    }}
                   />
+                  {errors.fullName && (
+                    <label className="block font-medium text-sm mb-2 mt-3 text-red-600" htmlFor="fullName">
+                      {errors.fullName?.message?.toString()}
+                    </label>
+                  )}
                 </div>
                 <div className="mb-[30px] relative">
                   <input
                     style={{
                       boxShadow: 'none',
                     }}
-                    type="email"
+                    type="text"
                     className="outline-none shadow-none text-[#666] box-border w-full bg-[#fafafa00] h-[58px] rounded-0 border-[#f9fdfe] border-b-2 border-t-0 border-l-0 border-r-0 focus:border-b-2 focus:border-[#96c583] focus:placeholder-[#96c583]"
-                    placeholder="Email Address"
+                    placeholder={data?.object?.form?.input.PHONE?.label}
+                    {...register('phone')}
+                    onChange={(e) => {
+                      if (!e.target.value) {
+                        clearErrors('phone');
+                      }
+                    }}
                   />
+                  {errors.phone && (
+                    <label className="block font-medium text-sm mb-2 mt-3 text-red-600" htmlFor="phone">
+                      {errors.phone?.message?.toString()}
+                    </label>
+                  )}
+                </div>
+                <div className="mb-[30px] relative">
+                  <input
+                    style={{
+                      boxShadow: 'none',
+                    }}
+                    type="text"
+                    className="outline-none shadow-none text-[#666] box-border w-full bg-[#fafafa00] h-[58px] rounded-0 border-[#f9fdfe] border-b-2 border-t-0 border-l-0 border-r-0 focus:border-b-2 focus:border-[#96c583] focus:placeholder-[#96c583]"
+                    placeholder={data?.object?.form?.input.EMAIL?.label}
+                    {...register('email')}
+                    onChange={(e) => {
+                      if (!e.target.value) {
+                        clearErrors('email');
+                      }
+                    }}
+                  />
+                  {errors.email && (
+                    <label className="block font-medium text-sm mb-2 mt-3 text-red-600" htmlFor="email">
+                      {errors.email?.message?.toString()}
+                    </label>
+                  )}
                 </div>
                 <div className="mb-[30px] relative">
                   <textarea
@@ -62,11 +139,22 @@ export const Contact: React.FC<ContactProps> = (props) => {
                       boxShadow: 'none',
                     }}
                     className="outline-none shadow-none placeholder:indent-[10px] indent-[10px] placeholder:text-[16px] text-[#666] placeholder-pl-2 box-border w-full bg-[#fafafa00] h-[120px] rounded-0 border-[#f9fdfe] border-b-2 border-t-0 border-l-0 border-r-0 focus:border-b-2 focus:border-[#96c583] focus:placeholder-[#96c583]"
-                    placeholder="Message"
+                    placeholder={data?.object?.form?.input.MESSAGE?.label}
+                    {...register('message')}
+                    onChange={(e) => {
+                      if (!e.target.value) {
+                        clearErrors('message');
+                      }
+                    }}
                     defaultValue=""
                   />
+                  {errors.message && (
+                    <label className="block font-medium text-sm mb-2 mt-3 text-red-600" htmlFor="message">
+                      {errors.message?.message?.toString()}
+                    </label>
+                  )}
                 </div>
-              </form>
+              </div>
             </div>
             <div className="lg:pl-[50px] transition-[background,border,border-radius,box-shadow,transform] duration-300">
               <LandingPageButtonV1 type="submit">
@@ -76,7 +164,7 @@ export const Contact: React.FC<ContactProps> = (props) => {
               </LandingPageButtonV1>
             </div>
           </div>
-        </div>
+        </form>
       </div>
     </section>
   );
