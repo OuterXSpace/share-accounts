e3092d5e7142d5cf0bee84d5b7f2f024592db9c3
DT0998 <duongkytrieuvn@gmail.com>
2024-06-14 01:37:40 +0700

    feat: add title section

--- src/next-core-ui/landing-page/src/core-ui/v2/components/title-section/title-section.views.tsx
+++ src/next-core-ui/landing-page/src/core-ui/v2/components/title-section/title-section.views.tsx
@@ -4,7 +4,7 @@ export const TitleSection: React.FC<ITitleSectionProps> = (props) => {
   const { title } = props;
   return (
     <div className="section half-margin">
-      <div className="flex rounded-[50vw] border border-[0.8px] border-white uppercase justify-center px-[6vw] py-[2vw] md:px-[1vw] md:py-[1.2vw] lg:px-[0.8vw] lg:py-[0.3vw] gap-[0.7vw] items-center w-fit h-[1.8vw]">
+      <div className="flex rounded-[50vw] border border-[0.8px] border-white uppercase justify-center px-[1.5vw] py-[2vw] md:px-[1vw] md:py-[1.2vw] lg:px-[0.8vw] lg:py-[0.3vw] gap-[0.7vw] items-center w-fit h-[1.8vw]">
         <div className="bg-[#A2254B] rounded-[50vw] lg:w-[0.5vw] w-[1.4vw] lg:h-[0.5vw] h-[1.4vw] mr-[1vw] md:mr-[0.5vw] lg:mr-0 select-none">
           <h5>
             <br />‍
