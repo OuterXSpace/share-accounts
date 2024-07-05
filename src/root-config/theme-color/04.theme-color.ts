599c4c284fd9cbd371b3394ee54326ec267e6ed9
DT0998 <duongkytrieuvn@gmail.com>
2024-07-05 17:45:10 +0700

    fix: change theme and plugin tailwind to style in js

--- src/root-config/theme-color/04.theme-color.ts
+++ src/root-config/theme-color/04.theme-color.ts
@@ -119,9 +119,6 @@ module.exports = {
     )})`,
     'gradient-to-top-secondary': 'linear-gradient(181.06deg, #e10000 0.91%, #a50000 99.09%)',
   }),
-  content: {
-    'counter-link': 'counter(linkCounter, decimal-leading-zero)',
-  },
 };
 
 export {};
